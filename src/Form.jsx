415546d38af03cdd707dde9e9462dd771f9b4bcc
Frank Imbruglia <imbruglia100@gmail.com>
2024-05-14 02:00:22 +0100

    Started errors

--- src/Form.jsx
+++ src/Form.jsx
@@ -9,21 +9,30 @@ const Form = () => {
   const [role, setRole] = useState(''); //radio button
   const [bio, setBio] = useState('');
   const [subscribe, setSubscribe] = useState(true); //checkbox
-  
+  const [errors, setErrors] = useState({})
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    const errors = {}
+
+    if(!name) errors.name = 'Name is required'
+    if(!email) errors.email = 'Email is required'
+    if(email && !email.split('').include('@')) errors.email = 'Must be a valid email'
+    if(phone && !phone.match(/^\\(?([0-9]{3})\\)?[-.\\s]?([0-9]{3})[-.\\s]?([0-9]{4})$/)) errors.phone = 'Must be a valid phone number'
+    if(phone && !phoneType) errors.phoneType = "Must have a phone type when phone number is valid"
+    if(bio.length > 280) errors.bio = "Bio is too long. Must be less than 280 characters"
+
+    setErrors({errors})
   };
-  console.log(bio);
   return (
     <form onSubmit={handleSubmit}>
       <div className="input-label-div">
         <label htmlFor='name'>Name:</label>
-        <input id='name' type='text' value={name} onChange={(e) => setName(e.target.value)} />
+        <input id='name' required type='text' value={name} onChange={(e) => setName(e.target.value)} />
       </div>
       <div className="input-label-div">
         <label htmlFor='email'>Email:</label>
-        <input id='email' type='text' value={email} onChange={(e) => setEmail(e.target.value)} />
+        <input id='email' required type='text' value={email} onChange={(e) => setEmail(e.target.value)} />
       </div>
       <div className="input-label-div">
         <label htmlFor='phone'>Phone:</label>
